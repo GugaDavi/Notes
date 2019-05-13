f865015d13565fc35cacff04e44d8f5457bcccd4
GugaDavi <gguedes.tech@gmail.com>
2019-05-13 11:24:45 -0300

    Notes

--- home/NotesReact.js
+++ home/NotesReact.js
@@ -9,10 +9,24 @@ React:
 
   Pesquisamos por "React"
 
-  Depois inserimos os links no docuemnto que iremos utilizar por enquanto: //Usando React 15.1.0
+  Depois inserimos os links no docuemnto que iremos utilizar por enquanto: // Usando React 15.1.0
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js"></script>
+
+  Instalação Local React:
+
+  Utilizar o link diretamente na nossa aplicação implica em serios problemas, contudo podemos fazer a instalação
+  local para utilizarmos as ferramentas do React.
+
+  Utilizando o "npm" fazemos a instação do "react" e do "react-dom".
+
+  Dai salvamos em uma variavel local:
+
+    const React = require('react');
+    const ReactDOM = require('react-dom');
+
+  Dessa forma não precisamos mais dos links, melhorando o desenpenho da nossa aplicação.
 
   Podemos utilizar as ferramentas da biblioteca:
 
@@ -141,7 +155,7 @@ Webpack:
     3.1 - Parametros do entry (Onde e qual arquivo será lido):
       3.1.1 - __dirname - Diretorio onde está localizado o arquivo;
       3.1.2 - 'src' - Pasta onde estará o arquivo;
-      3.1.3 - 'index' - Nome do arquiv;
+      3.1.3 - 'index' - Nome do arquivo;
     3.2 - Propriedades do output (Local e arquivo destino):
       3.2.1 - __dirname - Diretorio onde está localizado o arquivo;
       3.2.2 - 'dist' - Local onde será salvo o arquivo;
@@ -150,5 +164,62 @@ Webpack:
   5 - Instala o webpack Global;
   6 - Executamos o webpack no terminal;
 
+  Depois dessa execução o arquivo arquvivo "bundle.js" estará pronto.
+
+  Outra funcionalidade que utilizaremos é o "webpack-dev-server". Ferramente utilizada para monitorar os arquivos.
+
+  1 - Instalamos o webpack-dev-server;
+  2 - Editamos o arquivo webpack.config.js, acrescentamos no output:
+
+  publicPath: "/static/"
+
+  3 - Alteramos o nosso index.html, para utilziar o "./static/bundle.js";
+  4 - Desse modo sempre que fizermos alteração em um arquivo o servidor fará a transposição automatica.
+  5 - Basta atualizar o browser;
+  6 - No final do processo, damos um unico "webpack" e geramos o arquivo final;
+  7 - Ou colocamos o arquivo em
+
+  Modularização:
+
+  Para trabalharmos com funções de outras aplicações precisamos exporta-las e importa-las. Para tal fazemos:
+
+  function sum(x, y) {
+    return x + y;
+  }
+
+  module.exports = sum;
+
+  No arquvio que queremos exportar informamos com o "modules.exports";
+
+  No arquivo que iremos utilizar:
+
+  const sum = require('./app');
+
+  criamos uma variavel que irá receber o metodo, usando o "requie()" passando o local do arquivo;
+
+  Para utilização do ES6 precisamos fazer algumas alterações nas configurações:
+
+  Intalar as dependencias do babel:
+    babel-core@6
+    babel-loader@6
+    babel-preset-es2015@6
+    babel-preset-stage-0@6
+
+  Com as dependencias instaladas fazemos uma alteração no webpack.config.js, inserimos:
+
+  module: {
+    loaders: [{
+      test: /\.js$/, // Tipos dos arquivos que serão reenderizados;
+      exclude: /node_modules/, // Arvios ou pastas que serão excluidas da busca;
+      inclue: /src/, // Pasta principal;
+      loader: 'babel', // Conversor do ES5 para ES6
+    }]
+  }
+
+  Depois fazemos a configuração do .babelrc e inserimos:
+
+  {
+    "presets": ["es2015", "stage-0"],
+  }
 
 */
